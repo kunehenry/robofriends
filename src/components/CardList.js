81744a451f5797909b849766d769fe15520469f8
Dat Nguyen <dtng19@gmail.com>
2022-02-11 09:36:00 -0600

    deleting throw error in Cardlist.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,10 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-    //throwing error for ErrorBoundary to catch
-    if(true) {
-        throw new Error("error");
-    }
     return (
         <div>
             {
