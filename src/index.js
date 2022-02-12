44af8e2e795ec6312839ccb4f25d49701cebe4d3
kunehenry <dtng19@gmail.com>
2022-02-11 22:03:52 -0600

    adding deploying our react app lesson

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from "./containers/App";
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
 
-//these are the new syntax for React v18
-//https://github.com/reactwg/react-18/discussions/5
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>);
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 reportWebVitals();
